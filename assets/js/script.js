8d8a7341f0ff90a3448bd851b293a248baa772e8
Dr.Robotnik <jackj.1988@googlemail.com>
2023-12-17 20:09:58 +0000

    improved layout of page and created a clear history button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
-// TO DO
+// TO DO - readme and comment the code
 
-// README
-// capitalise first letter of the forecast divs
+// WAYS TO IMPROVE
+// - VALIDATION - IF USER DOESN'T ENTER A CITY A MODAL / ALERT SAYS "CITY NOT FOUND"  THE BELOW WORKS BUT BREAKS EVERYTHING IF IT PUT IT IN
+// if (data.message) {
+//         alert("City not found")
+//     }
+// - WHEN REFRESH IS CLICKED, THE HISTORY DOESN'T DISAPPEAR
+// - SHOW AVERAGE TEMPS, NOT JUST THE TEMPERATURE FOR THAT 3 HOUR PERIOD ON THAT DAY
+// - THE NUMBER OF CITIES GETS INFINITELY LONG - REACH A POINT WHERE THE  OLDEST AUTOMATICALLY DELETES
+// - DON'T APPEND HISTORY BUTTON IF IT ALREADY EXISTS
 
 $("#search-button").on("click", function (event) {
     event.preventDefault();
@@ -10,7 +17,7 @@ $("#search-button").on("click", function (event) {
     let forecastWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?appid=87838a9c0968d060b8db54b60caf3669&units=metricc&q=" + cityName;
     fetch(currentWeatherURL)
     .then(function (response) {
-        return response.json();
+        return response.json()
     })
     .then(function (data) {
         localStorage.setItem(cityName + ":currentWeather", JSON.stringify(data))
@@ -24,7 +31,7 @@ $("#search-button").on("click", function (event) {
 
         printDataToScreen(cityName);
         
-        let button = $("<button>").text(cityName).on("click", function() {
+        let button = $("<button>").addClass("mb-3").text(cityName).on("click", function() {
             printDataToScreen(cityName)
         })
         $("#history").append(button)
@@ -40,10 +47,10 @@ function printCurrentWeather(cityName, data) {
     let localUnixTime = data.dt + data.timezone // The UNIX time is based on GMT. The timezone data is the number of seconds away from GMT so needs to be added to the UNIX time to generate the correct local time
     let currentTime = dayjs.unix(localUnixTime).format("dddd DD MMMM HH:mm")
     let currentWeatherDiv = $("<div>").addClass("border border-secondary border-4 rounded-end-pill")
+    let currentWeatherHeader = $("<h2>").text(cityName + ", " + data.sys.country + ", " + currentTime)
     let currentWeatherIconCode = data.weather[0].icon
     let currentWeatherIcon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + currentWeatherIconCode + "@2x.png")
-    let currentWeatherHeader = $("<h2>").text(cityName + ", " + data.sys.country + ", " + currentTime)
-    let currentWeather = $("<p>").attr("id", "description").text(data.weather[0].description)
+    let currentWeather = $("<p>").addClass("description").text(data.weather[0].description)
     let currentTemp = $("<p>").text("Temperature: " + data.main.temp + " °C")
     let currentWind = $("<p>").text("Wind Speed: " + data.wind.speed + " metres/second")
     let currentHumidity = $("<p>").text("Humidity: " + data.main.humidity + "%")
@@ -56,13 +63,13 @@ function printForecast(data) {
     for (i = 7; i < data.list.length; i += 8) {
         // i initialised to 7 to get data for tomorrow and 8 is added to ensure the next day is added every time it loops through 
         // Average might be just for the 3 hour period though, so read documentation for how to find out the daily average values.
+        let forecastWeatherDiv = $("<div>").addClass("col-2 me-4 border border-secondary border-4 rounded")
         let localUnixTime = data.list[i].dt
         let forecastDate = dayjs.unix(localUnixTime).format("DD/MM/YY")
-        let forecastWeatherDiv = $("<div>").addClass("col-2 me-4 border border-secondary border-4 rounded")
+        let forecastWeatherHeader = $("<h4>").addClass("text-center").text(forecastDate)
         let forecastWeatherIconCode = data.list[i].weather[0].icon
         let forecastWeatherIcon = $("<img>").addClass("mx-auto d-block").attr("src", "https://openweathermap.org/img/wn/" + forecastWeatherIconCode + "@2x.png")
-        let forecastWeatherHeader = $("<h4>").addClass("text-center").text(forecastDate)
-        let forecastWeather = $("<p>").text(data.list[i].weather[0].description)
+        let forecastWeather = $("<p>").addClass("description").text(data.list[i].weather[0].description)
         let forecastTemp = $("<p>").text("Temp: " + data.list[i].main.temp + " °C")
         let forecastWind = $("<p>").text("Wind: " + data.list[i].wind.speed + " m/s")
         let forecastHumidity = $("<p>").text("Humidity: " + data.list[i].main.humidity + "%")
@@ -70,3 +77,8 @@ function printForecast(data) {
         $("#forecast").append(forecastWeatherDiv)
     }
 }
+
+$("#clear").on("click", function() {
+    localStorage.clear()
+    $(".forecast").html("")
+})
